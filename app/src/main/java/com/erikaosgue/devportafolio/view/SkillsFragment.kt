e25bf0d321259bf1dc721afe912c340bae495fcf
erikaosgue <1476@holbertonschool.com>
2021-04-16 18:20:40 -0500

    Add the Skills Fragment

---
+++ app/src/main/java/com/erikaosgue/devportafolio/view/SkillsFragment.kt
@@ -0,0 +1,23 @@
+package com.erikaosgue.devportafolio.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.erikaosgue.devportafolio.R
+
+
+class SkillsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_skills, container, false)
+    }
+
+
+}
